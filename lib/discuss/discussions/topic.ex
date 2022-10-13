2215bc145ee1555d5ee2e42805ddfcf06e529d7e
Timothy Barmann <tbarmann92@gmail.com>
2022-10-13 14:20:09 -0400

    use same variable names as the tutorial

--- lib/discuss/discussions/topic.ex
+++ lib/discuss/discussions/topic.ex
@@ -3,15 +3,15 @@ defmodule Discuss.Discussions.Topic do
   import Ecto.Changeset
 
   schema "topics" do
-    field :title, :string
+    field(:title, :string)
 
     timestamps()
   end
 
   @doc false
-  def changeset(topic, attrs) do
-    topic
-    |> cast(attrs, [:title])
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
     |> validate_required([:title])
   end
 end
