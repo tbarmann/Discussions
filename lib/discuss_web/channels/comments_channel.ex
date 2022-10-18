e8cb233e6d74a1e7be23952efc7df6c0adf0dc8f
Timothy Barmann <tbarmann92@gmail.com>
2022-10-18 12:45:46 -0400

    get websockets working

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,32 +1,17 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
 
-  @impl true
-  def join("comments:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  # socket is like the connection object in controllers;
+  # it represents the entire lifecycle of the socket
+  def join(name, _params, socket) do
+    {:ok, %{"hello" => "world"}, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comments:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+  def handle_in(name, message, socket) do
+    IO.puts("+++++++++")
+    IO.inspect(name)
+    IO.inspect(message)
+    IO.inspect(socket)
+    {:reply, :ok, socket}
   end
 end
